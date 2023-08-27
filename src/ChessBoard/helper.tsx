b9857b0ae1fa5826338563ff3c5dcf963572b63e
akash kumar <akashkumar@akashs-MacBook-Air.local>
2023-08-27 14:54:50 -0400

    dragging done

--- src/ChessBoard/helper.tsx
+++ src/ChessBoard/helper.tsx
@@ -16,8 +16,40 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
         event.returnValue=false
         drag.dragging=false;
         alert("mouseup")
+
+        let board=(document.querySelector("#chessBoard") as HTMLDivElement).getBoundingClientRect();
+        let box=document.querySelector(".chessBox");
+        let {top,left}=target.getBoundingClientRect();
+
+        let normalizedY=top-board.top;
+        let normalizedX=left-board.left;
+        let chessBoxW=box.getBoundingClientRect().width;
+        let chessBoxH=box.getBoundingClientRect().height;
+        
+        
+        let countY=Math.floor(normalizedY/chessBoxH)+1;
+        let countX=Math.floor((normalizedX/chessBoxW))+1
+        console.log(Math.floor(normalizedY/chessBoxH)+1)//(box.getBoundingClientRect().width)!==0?Math.floor(countOfBoxesY)+1:countOfBoxesY)
+        console.log(Math.floor((normalizedX/chessBoxW))+1)//(box.getBoundingClientRect().width)!==0?Math.round(countOfBoxesX)+1:countOfBoxesX);
+
+        let finalTop=board.top+ (countY-1)  *  chessBoxH;
+        let finalLeft=board.left+ (countX-1) *  chessBoxW;
+
+        let cur=position[target.id] ;
+        let yy=(finalTop-cur.top);
+        let xx=(finalLeft-cur.left);
+        
+        //console.log("target.style",target.style.border,target.style.transform,"top ",top," screenY",event.clientY,yy,xx);
+        //target.style.border="2px solid yellow";
+       target.style.transform= cur.transform+`translateY(${yy}px) translateX(${xx}px)`
+       
+       console.log("DROPPED inital ",cur.transform,target.style.transform,yy,xx,"top ", cur.top," left ",cur.left,finalTop,finalLeft,chessBoxH,chessBoxW)
+
+
+        
+        
         target.parentElement.removeEventListener("mouseup",mouseUp);
-        target.removeEventListener("mousemove",moving);
+        target.parentElement.removeEventListener("mousemove",moving);
     
         
     }
@@ -40,14 +72,14 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
              //target.style.border="2px solid yellow";
             target.style.transform= transform+`translateY(${yy}px) translateX(${xx}px)`
             
-            console.log("inital ",transform,yy,xx,"top ", top," left ",left,event.clientX,event.clientY);
+          //  console.log("inital ",transform,yy,xx,"top ", top," left ",left,event.clientX,event.clientY);
             
         }
 
         
     }
     
-    let target=event.target 
+    let target=event.target as HTMLDivElement
     
 
     console.log("!! target is ",target,event.currentTarget);
---
+++ src/learning.tsx
@@ -0,0 +1,7 @@
+let data=
+"\
+1. mouseup and mousemove works within the element to which they are attached only\
+2. getCoputed is changeing when styles are applied using js\
+3. absolute vs translate approach\
+4.currying\
+"

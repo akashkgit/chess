f3a3ee5ba18a5905eab5782a02148584478a502f
akash kumar <akashkumar@akashs-MacBook-Air.local>
2023-08-27 21:12:55 -0400

    combined drag and click feature

--- src/ChessBoard/ChessBoard.tsx
+++ src/ChessBoard/ChessBoard.tsx
@@ -1,11 +1,11 @@
-import {mouseDown} from "./helper";
+import {moveCoin,mouseDown,putPiece} from "./helper";
 import { initCoinPos } from "./types";
 import "./ChessBoard.scss"
 import React, { useEffect } from "react";
 import {boardMode,boxMap, mapping} from "../specs/data"
 let el:HTMLDivElement;
-let state:HTMLDivElement;
-let drag={dragging:false};
+let drag={dragging:false,click:false,el};
+let state=drag;
 
 let position:initCoinPos={};
 export function ChessBoard(){
@@ -20,7 +20,7 @@ export function ChessBoard(){
     },[])
     let color:boolean=true;
     console.log(boxMap)
-    return <div className="ChessBoard" id="chessBoard"  >
+    return <div className="ChessBoard" id="chessBoard"  onClick={(event)=>putPiece(event,state,position)} >
    {
     
    Array.from({length:8}).map((val,i)=>{
@@ -30,7 +30,8 @@ export function ChessBoard(){
     lColor=Number(!color)
     console.log(boxMap[i][j])
     let idx:string=(boxMap[i][j] as string);
-    return <div onMouseDown={(event:any)=>{mouseDown(event,drag,position);}} className={`chessBox  sq_${idx} box_${mapping[idx]!==undefined?mapping[idx]:"hidden"}`}   id={idx} key={String(String(i)+String(j))}>{"box" +boxMap[i][j]} </div>
+    
+    return <div     draggable  onClick={(event)=>moveCoin(event,drag,position)} onDragStart={(event:any)=>{mouseDown(event,drag,position);}} className={`chessBox sq_${idx} box_${mapping[idx]!==undefined?mapping[idx]:"hidden"}`}   id={idx} key={String(String(i)+String(j))}>{"box" +boxMap[i][j]} </div>
  })
    }).flat()}
     </div>
--- src/ChessBoard/helper.tsx
+++ src/ChessBoard/helper.tsx
@@ -1,12 +1,131 @@
+import {mapping} from "../specs/data"
 import { initCoinPos } from "./types";
+
 export function mouseOver(event:any){
 }
 
 
-export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
+export  let moveCoin=(event:any,state:{[k:string]:any},position:initCoinPos)=>{
     
+
+    
+    
+    
+        if(state.click===false){
+            console.log(" clicking to true");
+            
+            state.click=true;
+        
+            state.el=event.target;
+            event.stopPropagation();
+            }
+            else{
+                
+                state["dest"]=event.target;
+                console.log("Dest",state["dest"]);
+            }
+       
+       
+        
+        
+    
+    
+   
+
+        
+}
+
+export function putPiece(event:any,state:{[k:string]:any},position:initCoinPos){
+
+
+function approximate(){
+
+
+    let board=(document.querySelector("#chessBoard") as HTMLDivElement).getBoundingClientRect();
+    let box=document.querySelector(".chessBox");
+    let [top,left]=[event.clientY,event.clientX];
+
+    let normalizedY=top-board.top;
+    let normalizedX=left-board.left;
+    let chessBoxW=box.getBoundingClientRect().width;
+    let chessBoxH=box.getBoundingClientRect().height;
+    
+    
+    let countY=Math.floor(normalizedY/chessBoxH)+1;
+    let countX=Math.floor((normalizedX/chessBoxW))+1
+   // console.log(Math.floor(normalizedY/chessBoxH)+1)//(box.getBoundingClientRect().width)!==0?Math.floor(countOfBoxesY)+1:countOfBoxesY)
+    //console.log(Math.floor((normalizedX/chessBoxW))+1)//(box.getBoundingClientRect().width)!==0?Math.round(countOfBoxesX)+1:countOfBoxesX);
+
+    let finalTop=board.top+ (countY-1)  *  chessBoxH;
+    let finalLeft=board.left+ (countX-1) *  chessBoxW;
+
+    let cur=position[state.el.id] ;
+    let yy=(finalTop-cur.top);
+    let xx=(finalLeft-cur.left);
+    
+    //console.log("target.style",target.style.border,target.style.transform,"top ",top," screenY",event.clientY,yy,xx);
+    //target.style.border="2px solid yellow";
+   state.el.style.transform= cur.transform+`translateY(${yy}px) translateX(${xx}px)`
+   
+   
+
+
+
+}
+
+if(state.click===true){
+
+
+    if(!state.dest){
+    console.log("need to be placed");
+
+    approximate();
+
+
+   Object.keys((keys:string)=>{
+        let coin=document.querySelector(`#${keys}`);
+        let {top,left,bottom,right,width,height}=coin.getBoundingClientRect();
+        let elPos=(state.el as HTMLDivElement).getBoundingClientRect();
+
+
+        if(elPos.top>=top && elPos.bottom<=bottom && elPos.right<=right && elPos.left>=left)
+        alert("over an element");
+         
+        
+
+   })
+   state.click=false;
+}
+else{
+    let element=state.el as HTMLDivElement;
+    let dest=state.dest as HTMLDivElement;
+    console.log(dest.classList.toString().split(" "))
+    dest.classList.remove(...dest.classList.toString().split(" "))
+    dest.classList.add(...element.classList.toString().split(" "));
+    //let cb=document.querySelector("#chessBoard")
+    
+
+    element.classList.remove(element.classList.toString().split(" ")[2])
+    
+    console.log("bimage",element.style.backgroundImage);
+    state.click=false;
+}
+
+
+
+
+}
+}
+
+export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
+    event.preventDefault();
+  
+    
+
+    console.log(event.detail,"is the count")
+   
     drag.dragging=true;
-    console.log("mousedown");
+   console.log("mousedown");
     let style=false;
     
     function mouseUp(event:any){
@@ -15,8 +134,8 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
         event.cancelBubble=true;
         event.returnValue=false
         drag.dragging=false;
-        alert("mouseup")
-
+       alert("mouseup")
+//
         let board=(document.querySelector("#chessBoard") as HTMLDivElement).getBoundingClientRect();
         let box=document.querySelector(".chessBox");
         let {top,left}=target.getBoundingClientRect();
@@ -29,8 +148,8 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
         
         let countY=Math.floor(normalizedY/chessBoxH)+1;
         let countX=Math.floor((normalizedX/chessBoxW))+1
-        console.log(Math.floor(normalizedY/chessBoxH)+1)//(box.getBoundingClientRect().width)!==0?Math.floor(countOfBoxesY)+1:countOfBoxesY)
-        console.log(Math.floor((normalizedX/chessBoxW))+1)//(box.getBoundingClientRect().width)!==0?Math.round(countOfBoxesX)+1:countOfBoxesX);
+       // console.log(Math.floor(normalizedY/chessBoxH)+1)//(box.getBoundingClientRect().width)!==0?Math.floor(countOfBoxesY)+1:countOfBoxesY)
+        //console.log(Math.floor((normalizedX/chessBoxW))+1)//(box.getBoundingClientRect().width)!==0?Math.round(countOfBoxesX)+1:countOfBoxesX);
 
         let finalTop=board.top+ (countY-1)  *  chessBoxH;
         let finalLeft=board.left+ (countX-1) *  chessBoxW;
@@ -43,7 +162,9 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
         //target.style.border="2px solid yellow";
        target.style.transform= cur.transform+`translateY(${yy}px) translateX(${xx}px)`
        
-       console.log("DROPPED inital ",cur.transform,target.style.transform,yy,xx,"top ", cur.top," left ",cur.left,finalTop,finalLeft,chessBoxH,chessBoxW)
+       
+       
+      // console.log("DROPPED inital ",cur.transform,target.style.transform,yy,xx,"top ", cur.top," left ",cur.left,finalTop,finalLeft,chessBoxH,chessBoxW)
 
 
         
@@ -54,6 +175,7 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
         
     }
 
+   
     let  moving=function (event:any){
         
         event.preventDefault();
@@ -63,7 +185,7 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
         
         
         if(drag.dragging){
-            console.log("moving...");
+           console.log("moving...");
             let {top,left,x,y,transform}=position[target.id] ;
              let yy=(event.clientY-top);
              let xx=(event.clientX-left);
@@ -80,9 +202,8 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
     }
     
     let target=event.target as HTMLDivElement
-    
-
-    console.log("!! target is ",target,event.currentTarget);
+    console.log("mousedown");
+   // console.log("!! target is ",target,event.currentTarget);
     let parent=target.parentElement;
 
 
@@ -90,6 +211,7 @@ export function mouseDown(event:any,drag:{[k:string]:any},position:initCoinPos){
     parent.addEventListener("mouseup",mouseUp)
     
     parent.addEventListener("mousemove",moving)
+    
 
     
 }
